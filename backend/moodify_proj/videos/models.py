21c0006026184ba185930a06023224480e9b49ec
Sam <sam.eskandar00@hotmail.com>
2020-03-27 20:50:02 -0400

    setup reactapp

--- backend/moodify_proj/videos/models.py
+++ backend/moodify_proj/videos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
